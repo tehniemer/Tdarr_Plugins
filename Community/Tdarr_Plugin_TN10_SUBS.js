11c4c836c48d08f85a2998e3708696851a4c209f
tehniemer <jrniemand@gmail.com>
2022-11-29 09:43:33 -0600

    Update Tdarr_Plugin_TN10_SUBS.js

--- Community/Tdarr_Plugin_TN10_SUBS.js
+++ Community/Tdarr_Plugin_TN10_SUBS.js
@@ -2,7 +2,7 @@
 // tdarrSkipTest
 
 // Created by tehNiemer with thanks to drpeppershaker for the plugin
-// Tdarr_Plugin_rr01_drpeppershaker_extract_subs_to_SRT which served as the base for this.
+// Tdarr_Plugin_rr01_drpeppershaker_extract_subs_to_SRT which served as the building blocks.
 const details = () => ({
   id: 'Tdarr_Plugin_TN10_SUBS',
   Stage: 'Pre-processing',
